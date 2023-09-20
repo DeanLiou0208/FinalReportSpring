20bdb6692dd6d514b086f4a8c9174ddde3d51213
DeanLiou0208 <ikari0208@gmail.com>
2023-09-20 20:59:00 +0800

    第一次上傳 1.Spring boot 版本 3.1.3 2.Tomcat 10.1 3.eclipse 2023-09 4 JDK 17

---
+++ FinalReoprt/src/main/java/tw/ispan/eeit168/FinalReoprtApplication.java
@@ -0,0 +1,13 @@
+package tw.ispan.eeit168;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalReoprtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalReoprtApplication.class, args);
+	}
+
+}
---
+++ FinalReoprt/src/test/java/tw/ispan/eeit168/FinalReoprtApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.ispan.eeit168;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalReoprtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
