10859567ce5e13889ae9468fe100f6c1d09d61cb
DeanLiou <ikari0208@gmail.com>
2023-10-25 21:06:35 +0800

    202310 25

--- FinalReoprt/src/main/java/tw/ispan/eeit168/shop/controller/ProductSelectTypeController.java
+++ FinalReoprt/src/main/java/tw/ispan/eeit168/shop/controller/ProductSelectTypeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class ProductSelectTypeController {
 	@Autowired
 	private ProductSelectTypeService productSelectTypeService;
 	
-	@GetMapping(path ="/type")
+	@PostMapping(path ="/type")
 	public String findAll() {
 		
 		String findAll = productSelectTypeService.findAll();
